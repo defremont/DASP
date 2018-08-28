8b716b16bb2ec7341af9a32ca1568234ba1b4b49
unknown <andre.def93@gmail.com>
2018-08-28 14:52:38 -0300

    first commit

---
+++ src/containers/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { connect } from "react-redux";
+
+import Box from "grommet/components/Box";
+import Paragraph from "grommet/components/Paragraph";
+
+const Footer = () => (
+  <Box align="center">
+    <Paragraph>
+      &copy; 2018, <a href="#">GERCOM</a>
+    </Paragraph>
+  </Box>
+);
+
+function mapStateToProps(state) {
+  return {};
+}
+
+export default connect(mapStateToProps)(Footer);
